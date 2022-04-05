eadf2faea3ee974a4deb880eb8aba864e03910e4
s546553 <98134539+s546553@users.noreply.github.com>
2022-04-04 22:42:19 -0500

    Selector page added

--- routes/roasted.js
+++ routes/roasted.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('roasted', { title: 'Abhishek' });
+  res.render('roasted', { title: 'Search Results roasted class' });
 });
 
 
